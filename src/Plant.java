0e3c9160236c4fff89d6928cfec92120a93e1380
Simon Krol <simonkrol11@gmail.com>
2018-10-25 18:37:29 -0400

    modify parent methods

--- src/Plant.java
+++ src/Plant.java
@@ -1,9 +1,22 @@
 
 public class Plant extends Entity{
 	protected Spot location;
-	public Plant(int maxHP, int att, int def, double attSp, Lane lane, Spot location) {
+	protected int value;
+	
+	public Plant(int maxHP, int att, int def, double attSp, Lane lane) {
 		super(maxHP, lane, att, def, attSp);
+	}
+	public Spot getLocation()
+	{
+		return location;
+	}
+	public void setLocation(Spot location)
+	{
 		this.location = location;
+	}
+	public int getValue()
+	{
+		return value;
 	}
 
 }
