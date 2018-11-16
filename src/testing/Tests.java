706d275c22557f0c42b736816bea4279193025d7
Simon Krol <simonkrol11@gmail.com>
2018-11-16 13:33:33 -0500

    add test architecture

---
+++ src/testing/Tests.java
@@ -0,0 +1,15 @@
+package testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ //Put the name of the test class in here: TestZombie.class
+        })
+
+public class Tests
+{
+
+
+}
